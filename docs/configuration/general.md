323fde16af65943aff2baaeef36923ae74c5bd6c
Josh.5 <jsunnex@gmail.com>
2021-03-27 16:58:40 +1300

    Add initial documentation on the Unmanic Plugins system

--- docs/configuration/general.md
+++ docs/configuration/general.md
@@ -1,8 +1,10 @@
 ---
-title: General
+title: Configuration > General
 description: Unmanic Configuration - General
 id: general
 ---
+# (WIP)
+
 
 ![Settings - General](/img/unmanic-settings-general.jpg "settings-general")
 
--- sidebars.js
+++ sidebars.js
@@ -31,6 +31,25 @@ module.exports = {
         },
         {
             type: 'category',
+            label: 'Plugins',
+            items: [
+                'plugins/overview',
+                {
+                    type: 'category',
+                    label: 'Writing Plugins',
+                    items: [
+                        'plugins/writing_plugins/introduction',
+                        'plugins/writing_plugins/creating_a_new_plugin',
+                        'plugins/writing_plugins/plugin_settings',
+                        'plugins/writing_plugins/system_info',
+                        'plugins/writing_plugins/plugin_runner_types',
+                    ],
+                },
+                'plugins/plugin_manager_cli',
+            ],
+        },
+        {
+            type: 'category',
             label: 'Advanced',
             items: [
                 {
