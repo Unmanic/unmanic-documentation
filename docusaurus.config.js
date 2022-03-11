824ddf0f93a53f92647c9d51b9210da8f9a820ec
Josh.5 <jsunnex@gmail.com>
2022-03-11 19:59:50 +1300

    Remove breadcrumbs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,6 +80,7 @@ module.exports = {
             {
                 docs: {
                     sidebarPath: require.resolve("./sidebars.js"),
+                    breadcrumbs: false,
                 },
                 blog: {
                     showReadingTime: true,
